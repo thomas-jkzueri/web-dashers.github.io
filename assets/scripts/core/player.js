7c6f0324605654e80c72b982e1b6bfa1191ce651
thomas-jkzueri <thomrengg@icloud.com>
2026-05-08 18:24:35 +0200

    Implement touch event listeners for space key
    Add touch support for iPad to handle space key events.

--- assets/scripts/core/player.js
+++ assets/scripts/core/player.js
@@ -2788,3 +2788,13 @@ _updateBallJump(_0x2fe319) {
     this._waveTrail.reset();
   }
 }
+// Touch support for iPad
+document.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  document.dispatchEvent(new KeyboardEvent('keydown', { key: ' ', code: 'Space', keyCode: 32 }));
+}, { passive: false });
+
+document.addEventListener('touchend', function(e) {
+  e.preventDefault();
+  document.dispatchEvent(new KeyboardEvent('keyup', { key: ' ', code: 'Space', keyCode: 32 }));
+}, { passive: false });
